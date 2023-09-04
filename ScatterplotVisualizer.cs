a4ae9197301cbd36c194bc5aeb3a871ead84ea62
TopeOlafisoye <129410519+TopeOlafisoye@users.noreply.github.com>
2023-09-04 14:45:17 +0100

    Add files via upload

---
+++ ScatterplotVisualizer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DataPoint
+{
+    public float x;
+    public float y;
+    public float z;
+}
+
+public class ScatterplotVisualizer : MonoBehaviour
+{
+    public GameObject pointPrefab; // Assign your point prefab in the Inspector
+    public TextAsset dataJson;     // Assign your JSON data file in the Inspector
+
+    private List<DataPoint> dataPoints;
+
+    void Start()
+    {
+        LoadData();
+        CreateScatterplot();
+    }
+
+    void LoadData()
+    {
+        dataPoints = new List<DataPoint>();
+        dataPoints.AddRange(JsonUtility.FromJson<DataPoint[]>(dataJson.text));
+    }
+
+    void CreateScatterplot()
+    {
+        foreach (var dataPoint in dataPoints)
+        {
+            Vector3 position = new Vector3(dataPoint.x, dataPoint.y, dataPoint.z);
+            Instantiate(pointPrefab, position, Quaternion.identity);
+        }
+    }
+}
---
+++ TitanicDataVisualizer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.IO;
+
+public class TitanicDataVisualizer : MonoBehaviour
+{
+    public string csvFileName;
+    public GameObject dataPointPrefab;
+    public Transform dataPointsParent;
+
+    void Start()
+    {
+        string filePath = Path.Combine(Application.dataPath, csvFileName);
+        StreamReader reader = new StreamReader(filePath);
+
+        while (!reader.EndOfStream)
+        {
+            string line = reader.ReadLine();
+            string[] values = line.Split(',');
+
+            float x = float.Parse(values[0]);  // Example: Age
+            float y = float.Parse(values[1]);  // Example: Fare
+            // You can customize this to fit your dataset's structure
+
+            Vector3 position = new Vector3(x, 0, y);
+            Instantiate(dataPointPrefab, position, Quaternion.identity, dataPointsParent);
+        }
+
+        reader.Close();
+    }
+}
